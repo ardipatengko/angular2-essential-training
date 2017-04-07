bb5ec57447a8bec77c4ed7205fdf77844136a265
trainee <trainee>
2017-04-07 10:35:14 +0700

    add css and html

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app',
-  template: '<h1> My App </h1>'
+  template: 'app/app.component.html',
+  styles: ['app/app.component.css']
 })
 
 export class AppComponent {}
---
+++ app/media-item.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/common'
+
+@Component({
+  selector: 'mw-media-item',
+  templateUrl: 'app/media-item.component.html',
+  styleUrls: ['app/media-item.component.css']
+})
+
+export class MediaItemComponent { }
