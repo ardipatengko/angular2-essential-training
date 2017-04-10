b579ff12d6c5fd417522e63338df26140405a3d0
trainee <trainee>
2017-04-10 16:25:55 +0700

    final test

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -1,9 +1,12 @@
 import { Routes, RouterModule } from '@angular/router';
 
 import { MediaItemFormComponent } from './media-item-form.component';
+import { MediaItemListComponent } from './media-item-list.component';
 
 const appRoutes: Routes = [
-  { path: 'add', component: MediaItemFormComponent }
+  { path: 'add', component: MediaItemFormComponent },
+  { path: 'list', component: MediaItemListComponent },
+   { path: '', pathMatch: 'full', redirectTo: 'list' }
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
